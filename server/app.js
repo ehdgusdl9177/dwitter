aead9cd20ccb908f42a5eb3f3d5b1ba17ee93dd1
ehdgusdl9177 <k23129177@gmail.com>
2024-11-19 14:39:22 +0900

    create tweets router

--- server/app.js
+++ server/app.js
@@ -1,8 +1,10 @@
 import express from "express";
+import "express-async-errors";
 import cors from "cors";
 import morgan from "morgan";
 import helmet from "helmet";
-import "express-async-errors";
+
+import tweetRouter from "./router/tweets.js";
 
 const app = express();
 
@@ -10,5 +12,16 @@ app.use(express.json());
 app.use(helmet());
 app.use(cors());
 app.use(morgan("tiny"));
+
+app.use("/tweets", tweetRouter);
+
+app.use((req, res) => {
+  res.sendStatus(404);
+});
+
+app.use((error, req, res, next) => {
+  console.error(error);
+  res.sendStatus(500);
+});
 
 app.listen(8080);
