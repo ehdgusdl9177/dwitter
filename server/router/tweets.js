23280bd81a286ac53c10682db60dc2a4f25d6e01
ehdgusdl9177 <k23129177@gmail.com>
2024-11-25 14:39:37 +0900

    get username

--- server/router/tweets.js
+++ server/router/tweets.js
@@ -1,6 +1,39 @@
 import express from "express";
 import "express-async-errors";
 
+const tweets = [
+  {
+    id: "1",
+    text: "테스트 1",
+    createdAt: Date.now().toString(),
+    name: "Kevin",
+    username: "kevin",
+    url: "https://widgetwhats.com/app/uploads/2019/11/free-profile-photo-whatsapp-1.png",
+  },
+  {
+    id: "2",
+    text: "테스트 2",
+    createdAt: Date.now().toString(),
+    name: "Martin",
+    username: "martin",
+    url: "https://widgetwhats.com/app/uploads/2019/11/free-profile-photo-whatsapp-1.png",
+  },
+];
+
 const router = express.Router();
+
+// GET /tweets
+// GET /tweets?username=:username
+router.get("/", (req, res, next) => {
+  const username = req.query.username;
+  const data = username
+    ? tweets.filter((t) => t.username === username)
+    : tweets;
+  res.status(200).json(data);
+});
+// GET /tweets/:id
+// POST /tweets;
+// PUT /tweets/:id
+// DELETE /tweets/:id
 
 export default router;
