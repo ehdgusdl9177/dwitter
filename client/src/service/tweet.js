ae34af996bc09e5976abe2feb30654cc9974d7dc
ehdgusdl9177 <k23129177@gmail.com>
2024-11-26 13:46:06 +0900

    client

--- client/src/service/tweet.js
+++ client/src/service/tweet.js
@@ -2,8 +2,9 @@ export default class TweetService {
   constructor(baseURL) {
     this.baseURL = baseURL;
   }
+
   async getTweets(username) {
-    const query = username ? `?username=${username}` : "";
+    let query = username ? `?username=${username}` : "";
     const response = await fetch(`${this.baseURL}/tweets${query}`, {
       method: "GET",
       headers: { "Content-Type": "application/json" },
@@ -16,30 +17,36 @@ export default class TweetService {
   }
 
   async postTweet(text) {
-    const response = await fetch(`${this.baseURL}/tweets`, {
+    const response = await fetch(`${this.baseURL}/tweets/`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ text, username: "john", name: "John" }),
+      body: JSON.stringify({
+        text,
+        username: "ellie",
+        name: "Ellie",
+      }),
     });
     const data = await response.json();
     if (response.status !== 201) {
       throw new Error(data.message);
     }
+    console.log(data);
     return data;
   }
 
   async deleteTweet(tweetId) {
-    const response = await fetch(`${this.baseURL}/tweets${tweetId}`, {
+    const response = await fetch(`${this.baseURL}/tweets/${tweetId}`, {
       method: "DELETE",
       headers: { "Content-Type": "application/json" },
     });
     if (response.status !== 204) {
+      const data = await response.json();
       throw new Error(data.message);
     }
   }
 
   async updateTweet(tweetId, text) {
-    const response = await fetch(`${this.baseURL}/tweets${tweetId}`, {
+    const response = await fetch(`${this.baseURL}/tweets/${tweetId}`, {
       method: "PUT",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ text }),
