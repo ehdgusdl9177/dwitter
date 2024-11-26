8e617467c98b2c38693e5926c53641c3b2a3476a
ehdgusdl9177 <k23129177@gmail.com>
2024-11-26 13:47:06 +0900

    update server

--- server/app.js
+++ server/app.js
@@ -3,8 +3,7 @@ import "express-async-errors";
 import cors from "cors";
 import morgan from "morgan";
 import helmet from "helmet";
-
-import tweetRouter from "./router/tweets.js";
+import tweetsRouter from "./router/tweets.js";
 
 const app = express();
 
@@ -13,9 +12,9 @@ app.use(helmet());
 app.use(cors());
 app.use(morgan("tiny"));
 
-app.use("/tweets", tweetRouter);
+app.use("/tweets", tweetsRouter);
 
-app.use((req, res) => {
+app.use((req, res, next) => {
   res.sendStatus(404);
 });
 
@@ -23,5 +22,4 @@ app.use((error, req, res, next) => {
   console.error(error);
   res.sendStatus(500);
 });
-
 app.listen(8080);
--- server/router/tweets.js
+++ server/router/tweets.js
@@ -4,22 +4,20 @@ import "express-async-errors";
 let tweets = [
   {
     id: "1",
-    text: "테스트 1",
+    text: "드림코더분들 화이팅!",
     createdAt: Date.now().toString(),
-    name: "Kevin",
-    username: "kevin",
+    name: "Bob",
+    username: "bob",
     url: "https://widgetwhats.com/app/uploads/2019/11/free-profile-photo-whatsapp-1.png",
   },
   {
     id: "2",
-    text: "테스트 2",
+    text: "안뇽!",
     createdAt: Date.now().toString(),
-    name: "Martin",
-    username: "martin",
-    url: "https://widgetwhats.com/app/uploads/2019/11/free-profile-photo-whatsapp-1.png",
+    name: "Ellie",
+    username: "ellie",
   },
 ];
-
 const router = express.Router();
 
 // GET /tweets
@@ -27,7 +25,7 @@ const router = express.Router();
 router.get("/", (req, res, next) => {
   const username = req.query.username;
   const data = username
-    ? tweets.filter((t) => t.username === username)
+    ? tweets.filter((tweet) => tweet.username === username)
     : tweets;
   res.status(200).json(data);
 });
@@ -35,7 +33,7 @@ router.get("/", (req, res, next) => {
 // GET /tweets/:id
 router.get("/:id", (req, res, next) => {
   const id = req.params.id;
-  const tweet = tweets.find((t) => t.id === id);
+  const tweet = tweets.find((tweet) => tweet.id === id);
   if (tweet) {
     res.status(200).json(tweet);
   } else {
@@ -43,25 +41,25 @@ router.get("/:id", (req, res, next) => {
   }
 });
 
-// POST /tweets;
+// POST /tweeets
 router.post("/", (req, res, next) => {
   const { text, name, username } = req.body;
   const tweet = {
-    id: String(tweets.length + 1),
+    id: Date.now().toString(),
     text,
     createdAt: new Date(),
     name,
     username,
   };
-  tweets = [...tweets, tweet];
-  res.status(201).json(tweets);
+  tweets = [tweet, ...tweets];
+  res.status(201).json(tweet);
 });
 
 // PUT /tweets/:id
 router.put("/:id", (req, res, next) => {
   const id = req.params.id;
   const text = req.body.text;
-  const tweet = tweets.find((t) => t.id === id);
+  const tweet = tweets.find((tweet) => tweet.id === id);
   if (tweet) {
     tweet.text = text;
     res.status(200).json(tweet);
@@ -73,7 +71,7 @@ router.put("/:id", (req, res, next) => {
 // DELETE /tweets/:id
 router.delete("/:id", (req, res, next) => {
   const id = req.params.id;
-  tweets = tweets.filter((t) => t.id !== id);
+  tweets = tweets.filter((tweet) => tweet.id !== id);
   res.sendStatus(204);
 });
 
