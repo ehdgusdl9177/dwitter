76d0363e98f6232ced6b8f2e28f6379fb761965c
ehdgusdl9177 <k23129177@gmail.com>
2024-11-29 16:27:19 +0900

    repectoring client updateTweet

--- client/src/service/tweet.js
+++ client/src/service/tweet.js
@@ -28,15 +28,9 @@ export default class TweetService {
   }
 
   async updateTweet(tweetId, text) {
-    const response = this.http.fetch(`/tweets/${tweetId}`, {
+    return this.http.fetch(`/tweets/${tweetId}`, {
       method: "PUT",
-      headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ text }),
     });
-    const data = await response.json();
-    if (response.status !== 200) {
-      throw new Error(data.message);
-    }
-    return data;
   }
 }
