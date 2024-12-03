e8b4334d9062131a493ccf326ac4552552b1cba8
ehdgusdl9177 <k23129177@gmail.com>
2024-12-03 15:29:51 +0900

    create login validate

--- server/router/auth.js
+++ server/router/auth.js
@@ -6,4 +6,19 @@ import * as authController from "../controller/auth.js";
 
 const router = express.Router();
 
+const validateCredential = [
+  body("username")
+    .trim()
+    .notEmpty()
+    .withMessage("username should be at least 5 chracterts"),
+  body("password")
+    .trim()
+    .isLength({ min: 5 })
+    .withMessage("password should be at least 5 characters"),
+  validate,
+];
+
+router.post("/signup", authController.signup);
+router.post("/login", authController.login);
+
 export default router;
