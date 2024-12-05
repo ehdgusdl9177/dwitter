344952136d08fadc54707af0ed008e582e725928
ehdgusdl9177 <k23129177@gmail.com>
2024-12-05 14:29:06 +0900

    update client auth signup

--- client/src/service/auth.js
+++ client/src/service/auth.js
@@ -1,4 +1,23 @@
 export default class AuthService {
+  constructor(http, tokenStorage) {
+    this.http = http;
+    this.tokenStorage = tokenStorage;
+  }
+
+  async signup(username, password, name, email, url) {
+    const data = await this.http.fetch("/auth/signup", {
+      method: "POST",
+      body: JSON.stringify({
+        username,
+        password,
+        name,
+        email,
+        url,
+      }),
+    });
+    this.tokenStorage.saveToken(data.token);
+    return data;
+  }
   async login(username, password) {
     return {
       username: "kevin",
@@ -15,12 +34,5 @@ export default class AuthService {
 
   async logout() {
     return;
-  }
-
-  async signup(username, password, name, email, url) {
-    return {
-      username: "kevin",
-      token: "abc1234",
-    };
   }
 }
